b9fc741a6d9ed63e4d1d61fa8fb2465420b9380b
Emeric Bous <38536419+emericb@users.noreply.github.com>
2024-10-30 11:48:50 +0100

    add createRecipeWithIngredients
    add createRecipeWithIngredients

--- src/main/java/org/project/javaproject/repository/RecipeRepository.java
+++ src/main/java/org/project/javaproject/repository/RecipeRepository.java
@@ -1,7 +1,9 @@
 package org.project.javaproject.repository;
 
+import jakarta.transaction.Transactional;
 import org.project.javaproject.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +13,9 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT r FROM Recipe r JOIN r.products p WHERE p.name IN :ingredients")
     List<Recipe> findByIngredients(@Param("ingredients") List<String> ingredients);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO recipe_products (recipes_id, products_code) VALUES (:recipeId, :productCode)", nativeQuery = true)
+    void addProductToRecipe(@Param("recipeId") Long recipeId, @Param("productCode") String productCode);
 }
